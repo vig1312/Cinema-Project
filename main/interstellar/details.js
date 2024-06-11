df7f062d9c1803d4982465422aea72f5bb294ab5
hovo.vardanyan <hovo.vardanyan2003@gmail.com>
2024-06-11 16:59:51 +0400

    details

---
+++ main/interstellar/details.js
@@ -0,0 +1,20 @@
+let interstellar = {
+    year: 2014,
+    country: "USA",
+    genre: "fantasy",
+    producer: "Emma Thomas",
+    writer: "Jonathan Nolan",
+    director: "Christopher Nolan" 
+}
+    
+
+
+document.getElementById("year").innerHTML = interstellar.year;
+document.getElementById("country").innerHTML = interstellar.country;
+document.getElementById("genre").innerHTML = interstellar.genre;
+document.getElementById("director").innerHTML = interstellar.director;
+document.getElementById("producer").innerHTML = interstellar.producer;
+document.getElementById("writer").innerHTML = interstellar.writer;
+
+
+
